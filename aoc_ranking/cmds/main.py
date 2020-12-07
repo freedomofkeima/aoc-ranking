bae8475e72a06db3d174329e0cb601360bd1d5e5
Iskandar Setiadi <iskandarsetiadi@gmail.com>
2020-12-08 00:14:23 +0900

    Add real data from 2015

--- aoc_ranking/cmds/main.py
+++ aoc_ranking/cmds/main.py
@@ -3,16 +3,17 @@ import logging
 import sys
 
 from aoc_ranking.aoc import calculate_global_scoreboard, generate_global_rank
+from aoc_ranking.util import write_global_rank_to_file
 
 
 def main():
     # Logging
     logging.basicConfig(stream=sys.stdout, level=logging.INFO)
-    # TODO:
-    data = calculate_global_scoreboard(2020)
-    results = generate_global_rank(data)
-    for entity in results:
-        print(entity.rank, entity.name, entity.score, entity.photo_url)
+    # Generate cache
+    for year in range(2015, 2021):
+        data = calculate_global_scoreboard(year)
+        results = generate_global_rank(data)
+        write_global_rank_to_file(year, results)
 
 
 if __name__ == "__main__":
---
+++ aoc_ranking/util.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+import csv
+import os
+from typing import List
+
+from aoc_ranking.aoc import GlobalScoreRecord
+
+SCRIPT_PATH = os.path.abspath(os.path.dirname(__file__))
+
+
+def write_global_rank_to_file(year: int, data: List[GlobalScoreRecord]):
+    dst_path = os.path.join(SCRIPT_PATH, "..", "data", f"{year}.txt")
+    with open(dst_path, "w", newline="", encoding="utf-8") as fobj:
+        writer = csv.writer(fobj)
+        for entity in data:
+            writer.writerow(
+                [
+                    entity.rank,
+                    entity.name,
+                    entity.score,
+                    entity.photo_url if entity.photo_url else "",
+                ]
+            )
