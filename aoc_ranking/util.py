12483358ed31a2d78b9a73abe677ed0363f2566c
Iskandar Setiadi <iskandarsetiadi@gmail.com>
2020-12-08 01:06:01 +0900

    Update data

--- aoc_ranking/util.py
+++ aoc_ranking/util.py
@@ -12,6 +12,14 @@ def write_global_rank_to_file(year: int, data: List[GlobalScoreRecord]):
     dst_path = os.path.join(SCRIPT_PATH, "..", "data", f"{year}.txt")
     with open(dst_path, "w", newline="", encoding="utf-8") as fobj:
         writer = csv.writer(fobj)
+        writer.writerow(
+            [
+                "Rank",
+                "Name",
+                "Score",
+                "Avatar",
+            ]
+        )
         for entity in data:
             writer.writerow(
                 [
